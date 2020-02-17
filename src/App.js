b95ae31153b9f2e608555b6553386fd6fe1ec4aa
AlexandrDrachev <alexfront.front@gmail.com>
2020-02-17 16:59:24 +0200

    create start config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./components/button";
 
-function App() {
+const App = () => {
   return (
     <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
       <h1>Super cool page</h1>
@@ -10,6 +10,6 @@ function App() {
       </Button>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css' // <- change './index.css' to './styles.css'
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
